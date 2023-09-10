b4b95a4246a6524c178ec6014eaff0b6662243b2
yeua6321 <140486106+yeua6321@users.noreply.github.com>
2023-09-10 18:20:29 +0800

    Update server.js

--- files/server.js
+++ files/server.js
@@ -1,5 +1,6 @@
 const username = process.env.WEB_USERNAME || "admin";
 const password = process.env.WEB_PASSWORD || "password";
+const url = "http://127.0.0.1";
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
